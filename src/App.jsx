3fdb1926851d9dc2832ac5483a579afe9beb9c39
Naveen sai <naveensaikammela143@gmail.com>
2024-12-24 11:01:55 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import WeatherApp from "./weatherApp/WeatherApp";
 import Weat from "./clone/Weat";
 export default function App(){
-  return(<div>
+  return(<>
     <WeatherApp/>
     
-  </div>)
+  </>)
 }
